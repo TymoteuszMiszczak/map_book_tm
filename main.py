4e7cde664791b6e61567dc6b9ba25662f0039bb5
TymoteuszMiszczak <166720046+TymoteuszMiszczak@users.noreply.github.com>
2024-04-18 13:59:12 +0200

    feat:adeded create users

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.crud import read
+from utils.crud import read, create_user
 from models.data import users
 
 
@@ -10,8 +10,11 @@ if __name__ == "__main__":
         print("Menu:")
         print("0. Zakończ program: ")
         print("1.Pokaż co u znajomych: ")
+        print("2. Dodaj znajomego: ")
         menu_option:str=input("Wybierz dostępną funkcję z menu: ")
         if menu_option == "0":
             break
         if menu_option == "1":
             read(users)
+        if menu_option == "2":
+            create_user(users)
